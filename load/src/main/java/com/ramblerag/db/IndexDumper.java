c1d00cb242b80b2bcc468cf92f1df9c764dd0791
L.E. Mauget <mauget@gmail.com>
2012-07-08 19:09:39 -0400

    Reverted element names to those previous to most-recent change.

--- load/src/main/java/com/ramblerag/db/IndexDumper.java
+++ load/src/main/java/com/ramblerag/db/IndexDumper.java
@@ -43,11 +43,11 @@ public class IndexDumper {
 		out.writeCharacters(dir);
 		out.writeEndElement();
 		
-		out.writeStartElement("node-count");
+		out.writeStartElement("document-count");
 		out.writeCharacters(String.format("%d", reader.numDocs()));
 		out.writeEndElement();
 		
-		out.writeStartElement("nodes");
+		out.writeStartElement("document");
 		for (int i = 0; i < reader.numDocs(); i++) {
 			dumpDocument(reader.document(i), out);
 		}
